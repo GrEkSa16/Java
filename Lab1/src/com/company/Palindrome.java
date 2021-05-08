7d322e3046d67c726e5f8fa6543743e6388c8d44
Green <ggrreenn@bk.ru>
2021-05-08 13:52:31 +0300

    Lab1

--- Lab1/src/com/company/Palindrome.java
+++ Lab1/src/com/company/Palindrome.java
@@ -1,52 +1,52 @@
 package com.company;
-import java.util.Scanner;//подключаем класс сканера для получения информации с клавиатуры
+import java.util.Scanner;
 
 public class Palindrome
 {
     public static void main(String[] args)
     {
-        String n="";//создаем переменную для записи отдельных слов, записывем пустую строку
-        char w;//создаем переменную для записи отдельных букв
+        String str ="";//создаем переменную для записи отдельных слов, записывем пустую строку
+        char Letter;//создаем переменную для записи отдельных букв
         Scanner in = new Scanner(System.in);//создаем переменную типа Scanner для использования методов этого класса
-        System.out.println("Введите слова одной строкой через пробел");
+        System.out.println("Введите слова через пробел");
         String s = in.nextLine();//считываем введенную строку
         if(s.length() != 0)//если строка непустая, то
         {
             //проходим ко всем символам
             for (int i = 0; i < s.length(); i++)
             {
-                w = s.charAt(i);//записываем символ в переменную
-                if (w == ' ')//если символ является пробелом (слово закончилось)
+                Letter = s.charAt(i);//записываем символ в переменную
+                if (Letter == ' ')//если символ является пробелом (слово закончилось)
                 {
-                    System.out.println(n);//выводим слово
-                    if(isPalindrome(n))//проверяем является ли слово полиндромом
+                    System.out.println(str);//выводим слово
+                    if(isPalind(str))//проверяем является ли слово полиндромом
                         System.out.println('+');//если да, то пишем +
                     else
                         System.out.println('-');//иначе -
-                    n = "";//очищаем переменную со словом
+                    str = "";//очищаем переменную со словом
                 }
                 else//если символ не пробел, то добавляем его к слову
-                    n+=w;
+                    str += Letter;
             }
             //так как после последнего слова пробела нет, его нужно проверить отдельно
-            System.out.println(n);
-            if(isPalindrome(n))
-                System.out.println('+');
+            System.out.println(str);
+            if(isPalind(str))
+                System.out.println("yes");
             else
-                System.out.println('-');
+                System.out.println("no");
         }
     }
 
-    public static String reverseString(String s)//метод для переворачивания слова
+    public static String RString(String string)//метод для переворачивания слова
     {
-        String n="";//создаем переменную для записи перевернутого слова
-        for(int i=s.length()-1; i>=0; i--)//находим количество букв в слове
-            n+=s.charAt(i);//записываем буквы с конца
-        return (n);//возвращаем перевернутое слово
+        String revers   ="";//создаем переменную для записи перевернутого слова
+        for(int i = string.length()-1; i>=0; i--)//находим количество букв в слове
+            revers += string.charAt(i);//записываем буквы с конца
+        return (revers);//возвращаем перевернутое слово
     }
 
-    public static boolean isPalindrome(String s)//метод для проверки на "полиндромность"
+    public static boolean isPalind(String s)//метод для проверки на "палиндромность"
     {
-        return s.equals(reverseString(s));//возвращаем результат сравнение слова и результата переворачивающей функции
+        return s.equals(RString(s));//возвращаем результат сравнение слова и результата переворачивающей функции
     }
 }
